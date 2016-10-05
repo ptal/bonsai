8b646d1c9ed4e92fc76488480692a42815af10bd
Pierre Talbot <ptalbot@hyc.io>
2016-10-05 16:46:20 +0200

    [ChocoCubes][Example] Search engine based on first-fail for NQueens example.

--- examples/NQueens/src/main/java/chococubes/example/NQueens.java
+++ examples/NQueens/src/main/java/chococubes/example/NQueens.java
@@ -1,10 +1,13 @@
 package chococubes.example;
 
+import java.util.*;
 import java.lang.reflect.*;
 import inria.meije.rc.sugarcubes.*;
 import inria.meije.rc.sugarcubes.implementation.*;
 import org.chocosolver.solver.variables.*;
 import org.chocosolver.solver.constraints.nary.alldifferent.*;
+import org.chocosolver.solver.search.strategy.selectors.variables.*;
+import org.chocosolver.solver.search.strategy.selectors.values.*;
 import bonsai.chococubes.core.*;
 import bonsai.chococubes.choco.*;
 import bonsai.chococubes.sugarcubes.*;
@@ -15,16 +18,107 @@ public class NQueens
     Program p =
       new SpacetimeVar("domains", Spacetime.WorldLine, (env) -> VarStore.bottom(),
       new SpacetimeVar("constraints", Spacetime.WorldLine, (env) -> ConstraintStore.bottom(),
-      new SpacetimeVar("consistent", Spacetime.SingleTime, (env) -> FlatLattice.bottom(),
       SC.seq(
         model(),
-        propagation()))));
+        engine())));
     SpaceMachine machine = SpaceMachine.create(p);
     try {
       machine.execute();
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  // fn engine() {
+  //   trap FoundSolution {
+  //     par
+  //     || fail_first_middle();
+  //     || propagation();
+  //     || one_solution();
+  //     end
+  //   }
+  // }
+  private static Program engine() {
+    return
+      new SpacetimeVar("consistent", Spacetime.SingleTime, (env) -> FlatLattice.bottom(),
+      SC.until("FoundSolution",
+        SC.merge(
+          failFirstMiddle(),
+          propagation(),
+          oneSolution()
+        )));
+  }
+
+  // fn first_fail_middle() {
+  //   let var in single_space = new FirstFail(domains.model());
+  //   let val in single_space = new IntDomainMin();
+  //   loop {
+  //     when consistent |= Consistent.Unknown {
+  //       let x: IntVar in single_time = var.getVariable(domains.vars());
+  //       let mid: int in single_time = val.selectValue(domains[x]);
+  //       space
+  //       || constraints[domains] <- x.gt(mid);
+  //       || constraints[domains] <- x.lte(mid);
+  //       end
+  //     }
+  //     pause;
+  //   }
+  // }
+  private static Program failFirstMiddle() {
+    return
+      new SpacetimeVar("var", Spacetime.SingleSpace, (env) -> {
+        VarStore domains = (VarStore) env.var("domains");
+        return new FirstFail(domains.model());
+      },
+      new SpacetimeVar("val", Spacetime.SingleSpace, (env) -> new IntDomainMin(),
+      SC.loop(
+        SC.seq(
+        SC.when(new EntailmentConfig("consistent", (env) -> Consistent.Unknown),
+          new SpacetimeVar("x", Spacetime.SingleTime, (env) -> {
+            FirstFail var = (FirstFail) env.var("var");
+            IntDomainMin val = (IntDomainMin) env.var("val");
+            VarStore domains = (VarStore) env.var("domains");
+            return var.getVariable(domains.vars());
+          },
+          new SpacetimeVar("mid", Spacetime.SingleTime, (env) -> {
+            IntVar x = (IntVar) env.var("x");
+            IntDomainMin val = (IntDomainMin) env.var("val");
+            VarStore domains = (VarStore) env.var("domains");
+            return new Integer(val.selectValue((IntVar) domains.index(x)));
+          },
+          new Space(new ArrayList<>(Arrays.asList(
+            new SpaceBranch(new Tell("constraints", (env) -> {
+              IntVar x = (IntVar) env.var("x");
+              Integer mid = (Integer) env.var("mid");
+              return x.gt(mid);
+            })),
+            new SpaceBranch(new Tell("constraints", (env) -> {
+              IntVar x = (IntVar) env.var("x");
+              Integer mid = (Integer) env.var("mid");
+              return x.le(mid);
+            }))
+          ))))),
+          SC.nothing()),
+        SC.stop())
+      )));
+  }
+
+  // fn one_solution() {
+  //   loop {
+  //     when consistent |= Consistent.True {
+  //       exit FoundSolution;
+  //     }
+  //     pause;
+  //   }
+  // }
+  private static Program oneSolution() {
+    return SC.loop(
+      SC.seq(
+        SC.when(new EntailmentConfig("consistent", (env) -> Consistent.True),
+          SC.generate("FoundSolution"),
+          SC.nothing()),
+        SC.stop()
+    ));
   }
 
   // fn propagation() {
