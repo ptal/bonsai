3b6268318f7a5c09a41ebc0cf6c5c182bac34b55
Pierre Talbot <ptalbot@hyc.io>
2018-05-07 22:54:28 +0200

    [runtime][synchronous] Update the synchronous engine to push and pop onto a queue, start on the first instant, resuming after several instants and accessing variables.

---
+++ runtime/src/main/java/bonsai/runtime/synchronous/SpaceEnvironment.java
@@ -0,0 +1,162 @@
+// Copyright 2016 Pierre Talbot (IRCAM)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bonsai.runtime.synchronous;
+
+import java.util.*;
+import java.util.function.*;
+import bonsai.runtime.core.*;
+import bonsai.runtime.synchronous.variables.*;
+import bonsai.runtime.synchronous.statements.SpaceStmt;
+
+public class SpaceEnvironment<Queue extends Queueing<Future>>
+{
+  private HashMap<String, SingleTimeVar> varsST;
+  private HashMap<String, StreamVar> varsSS;
+  private HashMap<String, StreamVar> varsWL;
+  private HashMap<String, ModuleVar> varsModule;
+  private HashMap<String, Consumer<SpaceEnvironment>> refsUpdaters;
+
+  private Queue queue;
+
+  public SpaceEnvironment(Queue queue)
+  {
+    varsST = new HashMap();
+    varsSS = new HashMap();
+    varsWL = new HashMap();
+    varsModule = new HashMap();
+    refsUpdaters = new HashMap();
+    this.queue = queue;
+  }
+
+  /// We create and push the branches created in the current instant onto the queue.
+  /// It corresponds to `push: Store(L3) -> L4` in the lattice hierarchy.
+  /// Caution: this function must always be called even with an empty set of branches.
+  /// Rational: this function marks the end of an instant, and therefore internal data might be reinitialized.
+  /// @see Future
+  public void pushBranches(ArrayList<SpaceStmt> branches) {
+    ArrayList<Future> futures = Future.createFutures(branches, varsST, varsWL);
+    queue.push(futures);
+  }
+
+  public Optional<Future> pop() {
+    if(!queue.isEmpty()) {
+      Future future = queue.pop();
+      future.restoreWL(varsWL);
+      return Optional.of(future);
+    }
+    else {
+      return Optional.empty();
+    }
+  }
+
+  public HashMap<String, SingleTimeVar> swapVarsST(HashMap<String, SingleTimeVar> varsST) {
+    HashMap<String, SingleTimeVar> currentVarsST = this.varsST;
+    this.varsST = varsST;
+    return currentVarsST;
+  }
+
+  private <T extends Variable> void enterScope(T var, HashMap<String, T> vars) {
+    if (var == null) {
+      throw new RuntimeException("SpaceEnvironment.enterScope: null `var` parameter.");
+    }
+    T old = vars.put(var.uid(), var);
+    if (old != null) {
+      throw new RuntimeException(
+        "SpaceEnvironment.enterScope: The variable `" + var.name() +
+        "` (uid: `" + var.uid() + "`) is already in scope with the name `" + old.name() + "`.");
+    }
+  }
+
+  private <T extends Variable> void exitScope(String uid, HashMap<String, T> vars) {
+    if (uid == null) {
+      throw new RuntimeException("SpaceEnvironment.exitScope: null `uid` parameter.");
+    }
+    Variable removed = vars.remove(uid);
+    if (removed == null) {
+      throw new RuntimeException(
+        "SpaceEnvironment.exitScope: Try to exit the scope of the variable " +
+        "with uid: `" + uid + "` but it is not in scope.");
+    }
+  }
+
+  public void enterScopeST(SingleTimeVar var) { enterScope(var, this.varsST); }
+  public void enterScopeSS(StreamVar var) { enterScope(var, this.varsSS); }
+  public void enterScopeWL(StreamVar var) { enterScope(var, this.varsWL); }
+  public void enterScopeModule(ModuleVar var) { enterScope(var, this.varsModule); }
+  public void exitScopeST(String uid) { exitScope(uid, this.varsST); }
+  public void exitScopeSS(String uid) { exitScope(uid, this.varsSS); }
+  public void exitScopeWL(String uid) { exitScope(uid, this.varsWL); }
+  public void exitScopeModule(String uid) { exitScope(uid, this.varsModule); }
+
+  public void enterScopeRef(String uid, Consumer<SpaceEnvironment> refsUpdater) {
+    this.refsUpdaters.put(uid, refsUpdater);
+  }
+  public void exitScopeRef(String uid) {
+    this.refsUpdaters.remove(uid);
+  }
+
+  public Lattice latticeVar(String name, int time) {
+    Object value = var(name, time);
+    return Cast.toLattice(name, value);
+  }
+
+  public void checkVarNull(String uid, Variable v) {
+    if (v == null) {
+      throw new RuntimeException("The variable `" + uid
+        + "` is not registered in the environment.");
+    }
+  }
+
+  public void checkWrite(String uid, int time) {
+    if (time != 0) {
+      throw new RuntimeException("[BUG] The variable `" + uid
+        + "` is READ-ONLY because it is surrounded by `pre`.");
+    }
+  }
+
+  private Object access(String uid, int time) {
+    return null;
+    // Variable v = vars().get(uid);
+    // checkVarNull(uid, v);
+    // Generate an event on this variable to indicate it might has been modified.
+    // Note that `generatePure` does not read the new value of the variable yet–it is just used to wake up suspended statements—so it's OK to generate it before the actual modifications.
+    // if (permission != Permission.READ) {
+    //   checkWrite(uid, time);
+    //   Event event = getDirectAccessToEvent(new StringID(uid));
+    //   event.generatePure(this);
+    // }
+    // return v.value(time);
+  }
+
+  public Object var(String uid, int time) {
+    return access(uid, time);
+  }
+
+  public Object module(String uid) {
+    return varsModule.get(uid);
+  }
+
+  public boolean isEmpty() {
+    return queue.isEmpty();
+  }
+
+  public int queueSize() {
+    return queue.size();
+  }
+
+  // private HashMap<String, Variable> vars() {
+  //   return vars;
+  // }
+}
---
+++ runtime/src/main/java/bonsai/runtime/synchronous/SpaceMachine.java
@@ -0,0 +1,184 @@
+// Copyright 2016 Pierre Talbot (IRCAM)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// `SpaceMachine` is the class in charge of executing a spacetime program.
+/// The user control the execution of the program by calling the methods:
+///   * `macroStep()` to execute the program until it either `pause up`, `stop`, terminates or if the queue is empty.
+///   * `pop()` to prepare the program for the next instant.
+/// One must alternate the execution of `macroStep` and `pop` because the variables are only:
+///   * Readable after executing `macroStep()`: they represent the value computed until the end of the current instant.
+///   * Writeable after executing `pop()`: we prepared the variables for the next instant, and more information can be injected (but not read because the current instant has not been executed yet).
+/// For example, assuming we have a class `NQueens` computing the n-queens problem:
+/*
+  int N = 8;
+  NQueens nqueens = new NQueens(N);
+  int nodes = 0;
+  SpaceMachine<StackLR> machine = SpaceMachine.create(nqueens, new StackLR());
+  while (machine.macroStep()) {
+    // Print the intermediate solution.
+    System.out.println(nqueens);
+    machine.pop();
+    // Add some random constraints, just to show that constraint can be dynamically added during the exploration.
+    // The constraint forces the `nodes % N` queen to be less or equal to `nodes % N`.
+    nqueens.constraints().join(nqueens.queen(nodes % N).le(nodes % N));
+    nodes++;
+  }
+*/
+
+package bonsai.runtime.synchronous;
+
+import java.util.*;
+import bonsai.runtime.core.*;
+import bonsai.runtime.synchronous.interfaces.*;
+
+public class SpaceMachine<Queue extends Queueing<Future>>
+{
+  private Program body;
+  private SpaceEnvironment env;
+  private boolean firstInstant;
+  private boolean readyToStartInstant;
+
+  private boolean debug;
+  private int numReactions;
+
+  public static <Q extends Queueing<Future>> SpaceMachine<Q> create(Program body, Q queue) {
+    return new SpaceMachine(body, queue);
+  }
+
+  public static <Q extends Queueing<Future>> SpaceMachine<Q> createDebug(Program body, Q queue) {
+    SpaceMachine machine = SpaceMachine.create(body, queue);
+    machine.debug = true;
+    return machine;
+  }
+
+  public SpaceMachine(Program body, Queue queue) {
+    this.env = new SpaceEnvironment(queue);
+    debug = false;
+    numReactions = 0;
+    readyToStartInstant = false;
+    firstInstant = true;
+  }
+
+  // Returns `true` if the program is paused (through a `stop` or `pause up` statement).
+  // If the program is terminated or the queue empty, it returns `false`.
+  public boolean macroStep() {
+    log_debug_info("Start of execution");
+    firstPop();
+    CompletionCode code = CompletionCode.PAUSE;
+    while(code == CompletionCode.PAUSE && readyToStartInstant) {
+      code = react();
+    }
+    log_debug_info("End of execution (code: " + code + ")");
+    return code != CompletionCode.TERMINATE && !env.isEmpty();
+  }
+
+  private void firstPop() {
+    if(!firstInstant) {
+      pop();
+    }
+    else {
+      readyToStartInstant = true;
+      firstInstant = false;
+    }
+  }
+
+  private CompletionCode react() {
+    Result result = microStep();
+    env.pushBranches(result.branches());
+    CompletionCode code = result.code();
+    readyToStartInstant = false;
+    switch(code) {
+      case MICRO_STEP:
+        throw new RuntimeException(
+          "Completion code after a micro step cannot equal to `MICRO_STEP`.");
+      case PAUSE:
+        pop();
+        break;
+      default: break;
+    }
+    return code;
+  }
+
+  // This method is used for explicitly instantiating a future.
+  // Returns `true` if the machine is ready for a new instant, and `false` if no more future is available.
+  // If `pop()` has already been called during the current instant, a `RuntimeException` is thrown.
+  public boolean pop() {
+    if(!readyToStartInstant) {
+      Optional<Future> future = env.pop();
+      if (future.isPresent()) {
+        branchMicroStep(future.get());
+        readyToStartInstant = true;
+      }
+      return readyToStartInstant;
+    }
+    throw new RuntimeException(
+      "The method `SpaceMachine.pop()` has already in the current instant.\n" +
+      "Please call this method only one time per instant.");
+  }
+
+  private void log_debug_info(String status) {
+    if(debug) {
+      System.out.println("Reaction no " + numReactions);
+      System.out.println("  Execution status: " + status);
+      System.out.println("  Queue size: " + env.queueSize());
+    }
+  }
+
+  private Result microStep() {
+    numReactions++;
+    log_debug_info("Start of the instant");
+    Result result = new Result(CompletionCode.MICRO_STEP);
+    while(result.code() == CompletionCode.MICRO_STEP) {
+      result = body.execute(env);
+      switch(result.code()) {
+        case MICRO_STEP:
+          body.countReadWrite(env);
+          break;
+        default: break;
+      }
+    }
+    return result;
+  }
+
+  // We execute a branch process with the `single_time` variables associated to its `future`.
+  // The branch process must terminate immediately, and must not create branches.
+  private void branchMicroStep(Future future) {
+    future.swapVarsST(env);
+    Program branch = future.branch();
+    CompletionCode code = CompletionCode.MICRO_STEP;
+    while(code == CompletionCode.MICRO_STEP) {
+      Result result = branch.execute(env);
+      checkEmptyBranches(result);
+      code = result.code();
+      switch (code) {
+        case MICRO_STEP:
+          branch.countReadWrite(env);
+          break;
+        default: break;
+      }
+    }
+    future.swapVarsST(env);
+    if (code != CompletionCode.TERMINATE) {
+      throw new RuntimeException(
+        "A branch process must terminate immediately (code obtained: " + code + ")");
+    }
+  }
+
+  private void checkEmptyBranches(Result result) {
+    if (!result.branches().isEmpty()) {
+      throw new RuntimeException(
+        "A branch process must not create new branches (code obtained: " + result.code() + ")");
+    }
+  }
+}
