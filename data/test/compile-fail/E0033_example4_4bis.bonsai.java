acf91a2ff89dedbfc7fd79fa6d9607d2e17af956
Pierre Talbot <ptalbot@hyc.io>
2018-07-23 09:19:48 +0200

    [compiler][tests] Test the causality analysis (sequence, when then else).

---
+++ data/test/compile-fail/E0033_example4_4bis.bonsai.java
@@ -0,0 +1,33 @@
+// Copyright 2018 Pierre Talbot (IRCAM)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Variation of example 4.4 Section 4.5.4 in the dissertation (Talbot, 2018).
+
+#[error(E0033, 1, 0)]
+
+package test;
+
+public class E0033_example4_4bis
+{
+  public proc test() =
+    single_time LMax x = new LMax(0);
+    single_time LMax y = new LMax(0);
+    when x |= y then
+      x <- 2
+    else
+      y <- 3
+    end;
+    x <- 3;
+  end
+}
